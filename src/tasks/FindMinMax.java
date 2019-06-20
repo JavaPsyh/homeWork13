96b8306a6403989b6e6352aed9c5300e9b6c536a
JavaPsyh <psyhjava@gmail.com>
2019-06-20 13:36:48 +0300

    initial commit

---
+++ src/tasks/FindMinMax.java
@@ -0,0 +1,16 @@
+package tasks;
+
+import java.util.Comparator;
+import java.util.function.BiConsumer;
+import java.util.stream.Stream;
+
+public class FindMinMax {
+    public static <T> void findMinMax(
+            Stream<? extends T> stream,
+            Comparator<? super T> order,
+            BiConsumer<? super T, ? super T> minMaxConsumer) {
+
+
+
+    }
+}
---
+++ src/tasks/serialization/DeserializeAnimalArray.java
@@ -0,0 +1,8 @@
+package tasks.serialization;
+
+public class DeserializeAnimalArray {
+    public static Animal[] deserializeAnimalArray(byte[] data) {
+        Animal[] animals = new Animal[0];
+        return animals;
+    }
+}
