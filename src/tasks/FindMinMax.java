c74ade4fae3b1bb27a766554b6f781c015f7af00
JavaPsyh <psyhjava@gmail.com>
2019-06-20 22:30:26 +0300

    commit for verification

--- src/tasks/FindMinMax.java
+++ src/tasks/FindMinMax.java
@@ -1,7 +1,9 @@
 package tasks;
 
 import java.util.Comparator;
+import java.util.List;
 import java.util.function.BiConsumer;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class FindMinMax {
@@ -9,8 +11,9 @@ public class FindMinMax {
             Stream<? extends T> stream,
             Comparator<? super T> order,
             BiConsumer<? super T, ? super T> minMaxConsumer) {
-
-
-
+        List<? extends T> list = stream.collect(Collectors.toList());
+        T min = list.stream().min(order).orElse(null);
+        T max = list.stream().max(order).orElse(null);
+        minMaxConsumer.accept(min, max);
     }
 }
